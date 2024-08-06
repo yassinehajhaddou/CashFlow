514948aa60873ee8f118e8cbfca942f00ee289f1
yassinehajhaddou <yassinehajhaddou@gmail.com>
2024-08-06 14:35:58 +0200

    Create new Model for Financial Entry

---
+++ CashFlow/CashFlow/Model/FinancialEntry.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialEntry.swift
+//  CashFlow
+//
+//  Created by Yassine Haj Haddou on 06.08.24.
+//
+
+
+import Foundation
+struct FinancialEntry: Identifiable {
+ let id = UUID() // Eindeutige ID für den Monatseintrag
+ var month: Int // Monat
+ var items: [Item] // Liste der Posten für den Monat
+ var total: Double {
+  items.map { $0.amount }.reduce(0, +) // Berechnet die Gesamtsumme der Posten
+ }
+}
