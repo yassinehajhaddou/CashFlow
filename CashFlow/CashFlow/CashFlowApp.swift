9ece28d419949f0d50837c85d7198262593e674e
yassinehajhaddou <yassinehajhaddou@gmail.com>
2024-08-06 13:43:08 +0200

    initial Comit

---
+++ CashFlow/CashFlow/CashFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CashFlowApp.swift
+//  CashFlow
+//
+//  Created by Yassine Haj Haddou on 06.08.24.
+//
+
+import SwiftUI
+
+@main
+struct CashFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
