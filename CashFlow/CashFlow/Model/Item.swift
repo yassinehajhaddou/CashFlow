82d063c7fdae895ecec49a5e395c8c0616295198
yassinehajhaddou <yassinehajhaddou@gmail.com>
2024-08-06 14:34:43 +0200

    Create new Model for Item

---
+++ CashFlow/CashFlow/Model/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  CashFlow
+//
+//  Created by Yassine Haj Haddou on 06.08.24.
+//
+
+import Foundation
+struct Item: Identifiable {
+ let id = UUID() // Eindeutige ID für den Posten
+ var name: String // Name des Postens
+ var amount: Double // Betrag des Postens
+}
