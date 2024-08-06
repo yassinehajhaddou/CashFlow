1c58537c0c198ba8643802774341743343f0702a
yassinehajhaddou <yassinehajhaddou@gmail.com>
2024-08-06 14:41:23 +0200

    Setup NewCategoryView

--- CashFlow/CashFlow/NewCategoryView.swift
+++ CashFlow/CashFlow/NewCategoryView.swift
@@ -6,54 +6,69 @@
 //
 
 import SwiftUI
+
 struct NewCategoryView: View {
-    @State private var categoryName: String = ""
-    @State private var enableButton: Bool = false //  Button-Status speichern
+  @State private var categoryName: String = ""
     
-    var body: some View {
-        ZStack {
-            LinearGradient(gradient: Gradient(colors: [Color(UIColor(red: 42/255, green: 60/255, blue: 152/255, alpha: 1)), Color(UIColor(red: 22/255, green: 31/255, blue: 75/255, alpha: 1))]), startPoint: .topLeading, endPoint: .bottomTrailing)
-                .ignoresSafeArea()
-            
-            VStack {
-                Text("Neue Kategorie erstellen")
-                    .font(.title)
-                    .foregroundColor(.white)
-                    .padding(.top, 100)
-                
-                TextField("Kategorie eingeben", text: $categoryName)
-                    .font(.subheadline)
-                    .foregroundColor(Color.white)
-                    .padding()
-                    .frame(width: 290)
-                    .background(Color.white).opacity(0.3)
-                    .cornerRadius(10)
-                    .multilineTextAlignment(.center)
-                    .padding(.horizontal, 20)
-                
-                Spacer()
-                
-                Button(action: {
-                }) {
-                    Text("Speichern")
-                        .frame(width: 200, height: 50)
-                        .background(Color.white)
-                        .foregroundColor(Color(UIColor(red: 22/255, green: 31/255, blue: 75/255, alpha: 1)))
-                        .cornerRadius(10)
-                }
-                .padding(.bottom, 20)
-                
-                Button(action: {
-                }) {
-                    Text("Abbrechen")
-                        .foregroundColor(.white).opacity(0.5)
-                }
-                .padding(.bottom, 50)
-            }
+  @ObservedObject var viewModel: FinancialViewModel // Hinzufügen der viewModel-Zuordnung
+  var body: some View {
+      
+    ZStack {
+      LinearGradient(gradient: Gradient(colors: [Color(UIColor(red: 42/255, green: 60/255, blue: 152/255, alpha: 1)), Color(UIColor(red: 22/255, green: 31/255, blue: 75/255, alpha: 1))]), startPoint: .topLeading, endPoint: .bottomTrailing)
+        .ignoresSafeArea()
+        
+      VStack {
+        Text("Neue Kategorie erstellen")
+          .font(.title)
+          .foregroundColor(.white)
+          .padding(.top, 100)
+        TextField("Kategorie eingeben", text: $categoryName)
+          .font(.subheadline)
+          .foregroundColor(Color(UIColor(red: 22/255, green: 31/255, blue: 75/255, alpha: 1)))
+          .padding()
+          .background(Color.white)
+          .cornerRadius(10)
+          .multilineTextAlignment(.center)
+          .padding(.horizontal, 20)
+          
+        Spacer()
+          
+        Button(action: {
+          viewModel.addCategory(name: categoryName)
+          // Weitere Aktionen zum Speichern hinzufügen
+        }) {
+          Text("Speichern")
+            .frame(width: 200, height: 50)
+            .background(categoryName.isEmpty ? Color.gray .opacity(0.3) : Color.white)
+            .foregroundColor(categoryName.isEmpty ? Color.white : Color(UIColor(red: 22/255, green: 31/255, blue: 75/255, alpha: 1)))
+            .cornerRadius(10)
         }
+        .disabled(categoryName.isEmpty ? true : false)
+        .padding(.bottom, 20)
+          
+        Button(action: {
+          // Aktionen zum Abbrechen hinzufügen
+        }) {
+          Text("Abbrechen")
+            .foregroundColor(.white)
+        }
+        .padding(.bottom, 50)
+      }
     }
+  }
+}
+struct NewCategoryView_Previews: PreviewProvider {
+  static var previews: some View {
+    NewCategoryView(viewModel: FinancialViewModel()) // Übergabe von viewModel
+  }
 }
 
-#Preview {
-    NewCategoryView()
-}
+
+
+
+
+
+
+
+
+
