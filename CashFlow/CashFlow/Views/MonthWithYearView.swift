9cc9bd7758d5ab8b979eeb76ba99011ed4d9d771
Antonio Loggia <antonio.loggia@hotmail.de>
2024-08-08 23:09:04 +0200

    neue daten

---
+++ CashFlow/CashFlow/Views/MonthWithYearView.swift
@@ -0,0 +1,18 @@
+//
+//  YearMonthview.swift
+//  CashFlow
+//
+//  Created by Antonio Loggia on 08.08.24.
+//
+
+import SwiftUI
+
+struct YearMonthview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    YearMonthview()
+}
